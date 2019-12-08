992734a1d0f129acf8c6185349d96178550f552a
Jakub Woźniak <mescam@mescam.pl>
2019-12-08 01:32:11 +0100

    Fixed additional space in day1

--- day1.hs
+++ day1.hs
@@ -9,5 +9,5 @@ rocketFuel m = rocketFuelAcc m 0
 main :: IO()
 main = do
   content <- readFile "day1_input.txt"
-  let masses = map  (read::String->Int) (lines content)
+  let masses = map (read::String->Int) (lines content)
   print $ sum (map rocketFuel masses)
